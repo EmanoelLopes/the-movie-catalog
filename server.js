f4cb6f322d31a60388c55a794bf9fdf2086efcd8
Emanoel Lopes <emanoel.lopes.web@gmail.com>
2018-03-06 07:44:09 -0300

    fixing server

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
+// server.js
+
 const express = require('express');
-const path = require("path");
-const compression = require("compression");
+const path = require('path')
 const app = express();
 
-const forceSSL = function() {
-  return function(req, res, next) {
-    if (req.headers["x-forwarded-proto"] !== "https") {
-      return res.redirect(["https://", req.get("Host"), req.url].join(""));
-    }
-    next();
-  };
-};
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
-
-app.use(compression());
-app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
+
+console.log('App runing on port 8080');
