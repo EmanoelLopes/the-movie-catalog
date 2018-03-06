50a9e91304fe015b6dc8150d038b8f925c1620f8
emanoell <emanoel.lopes@me.com.br>
2018-03-06 19:18:13 -0300

    updating sw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 // Components
 import { AppComponent } from './app.component';
@@ -49,7 +50,7 @@ const AppRoutes = [
     BrowserModule,
     HttpModule,
     HttpClientModule,
-    ServiceWorkerModule.register('/ngsw-worker.js'),
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     RouterModule.forRoot(
       AppRoutes
     )
